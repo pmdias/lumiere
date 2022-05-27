9b8059be28be93855836bada7e6aa2efbae45583
Pedro Dias <pedrodias.miguel@gmail.com>
2022-05-27 23:54:11 +0200

    Add code that shoots rays into the scene

--- bin/main.ml
+++ bin/main.ml
@@ -1,17 +1,34 @@
+open Lumiere.Geometry
+
 type image_spec = { width : int; height : int }
 
 let write_output_header { width; height } =
   Printf.fprintf stdout "P3\n%d %d\n255\n" width height
 
+let ray_color (r : Lumiere.Ray.t) =
+  let unit_direction = Vec.norm r.dir in
+  let t = 0.5 *. (unit_direction.y +. 1.) in
+  (Vec.make 1. 1. 1. *: (1. -. t)) +: (Vec.make 0.5 0.7 1.0 *: t)
+
 let () =
-  let spec = { width = 256; height = 256 } in
+  let aspect_ratio = 16. /. 9. in
+  let spec = { width = 400; height = int_of_float (400. /. aspect_ratio) } in
+  let viewport_height = 2. in
+  let viewport_width = aspect_ratio *. viewport_height in
+  let focal_length = 1. in
+  let origin = Vec.make 0. 0. 0. in
+  let horizontal = Vec.make viewport_width 0. 0. in
+  let vertical = Vec.make 0. viewport_height 0. in
+  let lower_left_corner = origin -: horizontal /: 2. -: vertical /: 2. -: Vec.make 0. 0. focal_length in
   write_output_header spec;
   for j = spec.height - 1 downto 0 do
     Printf.fprintf stderr "\rScanlines remaining: %d " j;
     for i = 0 to spec.width - 1 do
-      let r = float_of_int i /. float_of_int (spec.width - 1) in
-      let g = float_of_int j /. float_of_int (spec.height - 1) in
-      Lumiere.Color.write_color (Lumiere.Geometry.Vec.make r g 0.25)
+      let u = float_of_int i /. float_of_int (spec.width - 1) in
+      let v = float_of_int j /. float_of_int (spec.height - 1) in
+      let r = Lumiere.Ray.make origin (lower_left_corner +: horizontal *: u +: vertical *: v -: origin) in
+      let color = ray_color r in
+      Lumiere.Color.write_color color
     done
   done;
   Printf.fprintf stderr "\nDone.\n"
