39cb0ab729880b2450864ac68811114c54018691
Pedro Dias <pedrodias.miguel@gmail.com>
2022-05-28 12:47:25 +0200

    Add generic hitrecord type

--- lib/ray.ml
+++ lib/ray.ml
@@ -1,5 +1,11 @@
 open Geometry
 
+module HitRecord = struct
+  type t = { p : Vec.t; normal : Vec.t; t : float }
+
+  let make p n t = { p; normal = n; t }
+end
+
 type t = { orig : Geometry.Point.t; dir : Geometry.Vec.t }
 
 let make o d = { orig = o; dir = d }
