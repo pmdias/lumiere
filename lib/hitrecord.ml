70a7596f3d33c63df6e70255768cc00a71ce0c47
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-06 20:13:13 +0200

    Refactor hit record type and functions

---
+++ lib/hitrecord.ml
@@ -0,0 +1,11 @@
+open Geometry
+
+type t = { point : Vec.t; normal : Vec.t; value : float; front_face : bool }
+
+let make point normal value = { point; normal; value; front_face = false }
+
+let set_front_face hrec (ray : Ray.t) outward_normal =
+  let front_face = Vec.dot ray.dir outward_normal < 0. in
+  let normal = if front_face then outward_normal else outward_normal *: -1. in
+  let new_rec = make hrec.point normal hrec.value in
+  { new_rec with front_face }
