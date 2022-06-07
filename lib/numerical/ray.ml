a50be11eb98fdb3f5b0665822ef00d3613fec67f
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-07 23:03:53 +0200

    Add ray type and functions to numerical library

---
+++ lib/numerical/ray.ml
@@ -0,0 +1,7 @@
+open Vector
+
+type t = { origin : Vec.t ; direction : Vec.t }
+
+let make origin direction = { origin; direction }
+
+let at r t = r.origin +: r.direction *: t
---
+++ lib/numerical/ray.mli
@@ -0,0 +1,16 @@
+open Vector
+
+type t = { origin : Vec.t ; direction : Vec.t }
+(** Record type that contains the information required to emulate a
+    ray to be shot into the scene. If consists of two vectors [Vec.t]
+    fields: an [origin] that specifies the origin point of the ray, and
+    a [direction] vector that gives the world direction where the ray
+    is shot *)
+
+val make : Vec.t -> Vec.t -> t
+(** Create a new [Ray.t] instance provided the two arguments, a [origin]
+   point, and a [direction] vector *)
+
+val at : t -> float -> Vec.t
+(** Given a ray [r] and a value [t], gives the point vector that matches
+    the location of the ray at that instant. *)
