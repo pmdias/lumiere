a5958add9d7ba10e68d409ccaad5bd95a1685254
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-08 20:22:51 +0200

    Apply format to the tracer module

--- lib/renderer/tracer.ml
+++ lib/renderer/tracer.ml
@@ -6,20 +6,13 @@ let trace_sample scene ray =
     |> List.map (fun x -> x ray 0.001 Float.infinity)
     |> List.filter (fun x -> match x with None -> false | _ -> true)
   in
-  match result with
-  | [] -> None
-  | x :: _ -> x
+  match result with [] -> None | x :: _ -> x
 
-let color_sample = function
-  | None -> Color.blue
-  | _ -> Color.red
+let color_sample = function None -> Color.blue | _ -> Color.red
 
 let trace_pixel scene output pixel_data =
-  let (_, _, x, y) = pixel_data in
+  let _, _, x, y = pixel_data in
   let camera = Scene.get_camera scene in
-  let u = float_of_int x /. (float_of_int @@ Output.get_width output - 1) in
-  let v = float_of_int y /. (float_of_int @@ Output.get_height output - 1) in
-  Camera.get_ray camera u v
-  |> trace_sample scene
-  |> color_sample
-
+  let u = float_of_int x /. (float_of_int @@ (Output.get_width output - 1)) in
+  let v = float_of_int y /. (float_of_int @@ (Output.get_height output - 1)) in
+  Camera.get_ray camera u v |> trace_sample scene |> color_sample
