c1cb76c1adda52851f064f431feaf0c098fb93b2
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-08 14:12:04 +0200

    Add numerical utility to clamp integer values

--- lib/numerical/utils.ml
+++ lib/numerical/utils.ml
@@ -1,4 +1,5 @@
 let clamp v min max = if v < min then min else if v > max then max else v
+let clamp_int v min max = clamp v min max
 
 let random_float a b =
   let breadth = b -. a in
--- lib/numerical/utils.mli
+++ lib/numerical/utils.mli
@@ -2,6 +2,9 @@ val clamp : float -> float -> float -> float
 (** Given a value [v], and a [max] and [min], it returns a new value which
     clamped between [max] and [min] *)
 
+val clamp_int : int -> int -> int -> int
+(** Similar to [clamp], but operates on values of type [int] *)
+
 val random_float : float -> float -> float
 (** Using a range [r] given by [a] and [b], where [r] includes both [a] and
     [b] and all numbers between them, returns a random number in [r]. *)
