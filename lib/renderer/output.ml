173e4e5f3dcb664bb703f27125311014a1c34a75
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-08 15:19:01 +0200

    Add function to output module to generate sequence of input pixel values

--- lib/renderer/output.ml
+++ lib/renderer/output.ml
@@ -7,3 +7,12 @@ let make width height =
 let get_width o = o.width
 let get_height o = o.height
 let get_aspect_ratio o = o.aspect_ratio
+
+let generate_pixels o =
+  let pixel_count = o.width * o.height in
+  let gen_pixel i =
+    let x = i mod o.width in
+    let y = i / o.width in
+    (pixel_count, i, x, y)
+  in
+  Seq.init pixel_count gen_pixel
--- lib/renderer/output.mli
+++ lib/renderer/output.mli
@@ -14,3 +14,10 @@ val get_height : t -> int
 
 val get_aspect_ratio : t -> float
 (** Get the aspect ratio of a target output *)
+
+val generate_pixels : t -> (int * int * int * int) Seq.t
+(** Returns a sequence of pixels [Seq.t] where each node of the sequence is
+    a tuple of four values, matching the total number of nodes in the sequence,
+    the current node index, the [x] coordinate of the pixel that matches the
+    current node, and the [y] coordinate of the pixel that matches the current
+    node. *)
