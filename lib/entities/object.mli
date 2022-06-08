643cf8f0cc71dc27ad44f77af8d4a456d13698c3
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-08 13:30:10 +0200

    Update object interface and add comments to types and functions

--- lib/entities/object.mli
+++ lib/entities/object.mli
@@ -1,5 +1,17 @@
+open Numerical
 open Numerical.Vector
 
 type t
+(** Opaque type that encapsulates an object that exists in the simulation world,
+    and therefore makes up part of the rendering scene *)
+
+type hittable = Ray.t -> float -> float -> Hitrecord.t option
+(** Type used to represent any shape that can be hit and therefore return
+    information regarding a specific ray hitting the shape. *)
+
+val hit_test : t -> hittable
+(** Access the hit test of a specific object [t] instance *)
 
 val make_sphere : Vec.t -> float -> t
+(** Create a new object [t] that emulates the behaviour of a sphere shaped
+    structure. *)
