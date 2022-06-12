f8d29d31e33eaa37453118827d34a809dc5e8eab
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-12 23:17:51 +0200

    Finish camera refactor to include defocus blur

--- lib/entities/camera.ml
+++ lib/entities/camera.ml
@@ -6,11 +6,15 @@ type t = {
   lower_left_corner : Vec.t;
   horizontal : Vec.t;
   vertical : Vec.t;
+  u : Vec.t;
+  v : Vec.t;
+  w : Vec.t;
+  lens_radius : float;
 }
 
 let degrees_to_radians degrees = degrees *. Float.pi /. 180.
 
-let make lookfrom lookat vup vfov aspect_ratio =
+let make lookfrom lookat vup vfov aspect_ratio aperture focus_dist =
   let theta = degrees_to_radians vfov in
   let h = Float.tan @@ (theta /. 2.) in
   let height = 2. *. h in
@@ -20,14 +24,18 @@ let make lookfrom lookat vup vfov aspect_ratio =
   let u = Vec.normalize @@ Vec.cross vup w in
   let v = Vec.cross w u in
 
+  let lens_radius = aperture /. 2. in
   let origin = lookfrom in
-  let horizontal = u *: width in
-  let vertical = v *: height in
+  let horizontal = u *: width *: focus_dist in
+  let vertical = v *: height *: focus_dist in
   let lower_left_corner =
-    origin -: (horizontal /: 2.) -: (vertical /: 2.) -: w
+    origin -: (horizontal /: 2.) -: (vertical /: 2.) -: (w *: focus_dist)
   in
-  { origin; lower_left_corner; horizontal; vertical }
+  { origin; lower_left_corner; horizontal; vertical; lens_radius; u; v; w }
 
-let get_ray c u v =
-  c.lower_left_corner +: (c.horizontal *: u) +: (c.vertical *: v) -: c.origin
-  |> Ray.make c.origin
+let get_ray c s t =
+  let rd = Utils.random_in_unit_disk () *: c.lens_radius in
+  let offset = (c.u *: rd.x) +: (c.v *: rd.y) in
+  c.lower_left_corner +: (c.horizontal *: s) +: (c.vertical *: t) -: c.origin
+  -: offset
+  |> Ray.make (c.origin +: offset)
--- lib/entities/camera.mli
+++ lib/entities/camera.mli
@@ -4,7 +4,7 @@ open Numerical.Vector
 type t
 (** Type structure that represents the camera used to render a scene. *)
 
-val make : Vec.t -> Vec.t -> Vec.t -> float -> float -> t
+val make : Vec.t -> Vec.t -> Vec.t -> float -> float -> float -> float -> t
 (** Create a new camera instance based on a specific aspect ratio *)
 
 val get_ray : t -> float -> float -> Ray.t
