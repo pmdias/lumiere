335d089fa399652c14312853d8021b7c7a976fca
Pedro Dias <pedrodias.miguel@gmail.com>
2022-06-15 19:59:26 +0200

    Refactor hitrecord to use Gg vector types and functions

--- lib/numerical/hitrecord.ml
+++ lib/numerical/hitrecord.ml
@@ -1,12 +1,12 @@
-open Vector
+open Gg
 
-type t = { point : Vec.t; normal : Vec.t; t : float; front_face : bool }
+type t = { point : V3.t; normal : V3.t; t : float; front_face : bool }
 
 let make point normal t front_face = { point; normal; t; front_face }
 
 let set_front_face (h : t) (r : Ray.t) n =
-  let front_face = Vec.dot r.direction n < 0. in
-  let normal = if front_face then n else Vec.negate n in
+  let front_face = V3.(dot r.direction n) < 0. in
+  let normal = if front_face then n else V3.(neg n) in
   { h with normal; front_face }
 
 let compare a b = if a.t > b.t then 1 else if a.t < b.t then -1 else 0
--- lib/numerical/hitrecord.mli
+++ lib/numerical/hitrecord.mli
@@ -1,14 +1,14 @@
-open Vector
+open Gg
 
-type t = { point : Vec.t; normal : Vec.t; t : float; front_face : bool }
+type t = { point : V3.t; normal : V3.t; t : float; front_face : bool }
 (** Record structure used to store the information regarding the intersection
     of a [Ray.t] with any structure in the scene *)
 
-val make : Vec.t -> Vec.t -> float -> bool -> t
+val make : V3.t -> V3.t -> float -> bool -> t
 (** Create a new [t] instance from a [point] vector, a [normal] vector, a [t]
     value, and a [front_face] boolean. *)
 
-val set_front_face : t -> Ray.t -> Vec.t -> t
+val set_front_face : t -> Ray.t -> V3.t -> t
 (** Given a hit record [h], a ray [r], and a normal vector [n], this function
     updates the hit record so that the normal that is stored inside is pointing
     outside the surface. *)
